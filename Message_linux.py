9c29c16bc0c90c37f3491075cb0b363b9dd4c512
Satyendra <pandey@localhost.localdomain>
2017-09-20 01:32:01 +0530

    Resolved Error on having more messages

--- Message_linux.py
+++ Message_linux.py
@@ -1,6 +1,9 @@
 from fbchat import Client, log
 from getpass import getpass
-import sys, os, urllib
+from datetime import datetime
+import sys, os, urllib, time, socket
+
+socket.setdefaulttimeout(60)
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
@@ -14,58 +17,91 @@ USER = client.fetchUserInfo(client.uid)[client.uid]
 self = USER.name
 
 docs = ['docx', 'doc', 'pdf', 'pptx', 'txt', 'xlsx']
-media = ['mp3', 'mp4']
+media = ['mp3', 'mp4', 'aac', 'webm', 'avi', '3gp']
+gen = ['jpg', 'png']
 
 def do_rest(thread):
 	data = str(thread).split(" ")
 	if len(data) == 4:
 		other = data[1] +  " " + data[2]
-
 		id = data[3].split('(')[1].split(')')[0]
-		folder_name = str(data[1]) + '_' + str(data[2])
+		folder_name = "Data/" + str(data[1]) + '_' + str(data[2])
 		Testing = Path_check(folder_name)
 		filename = folder_name+"/" + str(data[1]) + '_' + str(data[2]) + ".txt"
 		file = open(filename, 'wb')
-		messages = client.fetchThreadMessages(thread_id=id, limit=number)
-		messages.reverse()
-		for message in messages:
-			if message.text is not None:
-				if message.author == uid:
-					file.write(self + ' -- ' + message.text.encode('utf-8') + ' \n' )
-				else:
-					file.write(other + ' -- ' + message.text.encode('utf-8') + ' \n' )
-			if message.attachments:
-				for attachment in message.attachments:
-				# For Image
-					Filename = attachment['filename']
-					if  Filename.split("-")[0] == 'image':
-						add = attachment['large_preview']['uri']
-						name = folder_name +"/images/"+ attachment['filename']+'.'+attachment['original_extension']
-						urllib.urlretrieve(add, name)
-					elif len(Filename.split(".")) > 1 and Filename.split(".")[1] in docs:
-						add = attachment['url']
-						test = urllib.urlopen(add)
-						temp = test.read().split('replace("')[1]
-						temp = temp.split('");</script>')[0]
-						temp = temp.replace("\\","")
-						Filename = folder_name + "/docs/" + Filename
-						urllib.urlretrieve(temp, Filename)
-					elif len(Filename.split(".")) > 1 and Filename.split(".")[1] in media:
-						add = attachment['playable_url']
-						Filename = folder_name + "/media/" + Filename
-						urllib.urlretrieve(add, Filename)
+		flag = 1000
+		num = 0
+		timestamp = int(19800 + time.time())*1000
+		while( flag > 999):
+			messages = client.fetchThreadMessages(thread_id=id, limit=1000, before=timestamp)
+			timestamp = messages[len(messages)-1].timestamp
+			for message in messages:
+				if message.text is not None:
+					if message.author == uid:
+						file.write(self + ' -- ' + message.text.encode('utf-8') + ' \n' )
 					else:
-						add = attachment['url']
-						test = urllib.urlopen(add)
-						temp = test.read().split('replace("')[1]
-						temp = temp.split('");</script>')[0]
-						temp = temp.replace("\\","")
-						Filename = folder_name + "/Random/" + Filename
-						urllib.urlretrieve(temp, Filename)
+						file.write(other + ' -- ' + message.text.encode('utf-8') + ' \n' )
+				if message.attachments:
+					for attachment in message.attachments:
+					# For Image
+						time.sleep(.1)
+						Filename = attachment['filename']
+						if  Filename.split("-")[0] == 'image':
+							add = attachment['large_preview']['uri']
+							name = folder_name +"/images/"+ attachment['filename']+'.' +attachment['original_extension']
+							try:
+								urllib.urlretrieve(add, name)
+							except:
+								print "Getting some error now on url -: ", add
+						elif len(Filename.split(".")) > 1 and Filename.split(".")[len(Filename.split("."))-1] in docs:
+							add = attachment['url']
+							test = urllib.urlopen(add)
+							temp = test.read().split('replace("')[1]
+							temp = temp.split('");</script>')[0]
+							temp = temp.replace("\\","")
+							Filename = folder_name + "/docs/" + Filename
+							try:
+								urllib.urlretrieve(temp, Filename)
+							except:
+								print "Getting some error now on url -: ", temp
+						elif len(Filename.split(".")) > 1 and Filename.split(".")[len(Filename.split("."))-1] in media:
+							add = attachment['playable_url']
+							Filename = folder_name + "/media/" + Filename
+							try:
+								urllib.urlretrieve(add, Filename)
+							except:
+								print "Getting some error now on url -: ", add
+						elif Filename.split("-")[0] == 'gif':
+							add = attachment['animated_image']['uri']
+							Filename = folder_name + "/media/" + Filename
+							try:
+								urllib.urlretrieve(add, Filename)
+							except:
+								print "Getting some error now on url -: ", add
+						else:
+							add = attachment['url']
+							test = urllib.urlopen(add)
+							temp = test.read().split('replace("')[1]
+							temp = temp.split('");</script>')[0]
+							temp = temp.replace("\\","")
+							Filename = folder_name + "/Random/" + Filename
+							try:
+								urllib.urlretrieve(temp, Filename)
+							except:
+								print "Getting some error now on url -: ", temp
+			flag = len(messages)
+			num += flag
+			print num, " messages had been downloaded from today till - ",datetime.utcfromtimestamp(timestamp).strftime('%d-%m-%Y')
 		file.close()
 
 
 def Path_check(name):
+
+	path = "Data"
+	if not os.path.exists(path):
+		os.mkdir(path)
+
+	name = path + '/' +name
 
 	path = name
 	if not os.path.exists(path):
@@ -91,22 +127,20 @@ def Path_check(name):
 
 username = str(raw_input("want to download messages from a specific friend type(y/n): "))
 
-number = int(raw_input("Maximum number of messages from 1 person: "))
-
 if username.lower() == 'y':
-	names = str(raw_input("Name of that friends separated by a comma like - satyendra pandey, Narendra pandey ,... , ... "))
+	names = str(raw_input("Name of that friends separated by a comma like - satyendra pandey, Narendra pandey--: "))
 	names = names.split(',')
 	for name in names:
 		thread = client.searchForThreads(name)[0]
 		do_rest(thread)
 
-else: 
-	num = int(raw_input("Number of frieds messages you wanna download from your inbox top : "))
+else:
 
+	num = int(raw_input("Number of friends from top of your chatlist:"))
 	if num < 20:
 		threads = client.fetchThreadList(limit = num)
 
-	else:	
+	else:
 		threads = client.fetchThreadList(limit = 20)
 		num = (num-20)/20
 
@@ -114,5 +148,5 @@ else:
 			offset = 20*(i+1)
 			threads += client.fetchThreadList(offset = offset, limit= 20)
 
-		for thread in threads:
-			do_rest(thread)
+	for thread in threads:
+		do_rest(thread)
